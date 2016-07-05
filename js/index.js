158c54aabe7183c181bcc54b2bfa71929036d4f0
chechecarer <1357660470@qq.com>
2016-07-05 11:13:47 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-/**
- * Created by Administrator on 2016/05/25.
- */
+
+/*存储商品信息*/
 var data =  new Array();
 
+/*删除商品*/
 function delGoods(e){
     this.parentNode.parentNode.removeChild(this.parentNode);
     data.splice(this.index,1);
-    console.log(data);
 }
 
+/*添加商品*/
 function addGoods(){
     var goodsID = document.getElementById('goodsID').value.replace(/(^\s*)|(\s*$)/g, "");
     var goodsNum = document.getElementById('goodsNum').value.replace(/(^\s*)|(\s*$)/g,"");
@@ -26,19 +26,23 @@ function addGoods(){
 
 }
 
+/*提交操作*/
 function submit(){
-    console.log('submit form');
-    jQuery.getJSON('jsonData/goodsInfo.json',function(data){
-        console.log(data);
-    })
-//    var request = createRequest();
-//    if(request == null){
-//        console.log('unable to create request');
-//    }
-//    else{
-//        var url = '';
-//    }
+    /*格式化数据*/
+    var nData = '';
+    nData += '[{\"id\":\"'+data[0].id;
+    nData +='\",\"num\":'+data[0].num+'}';
+    for(var i = 1 ; i < data.length ; i++){
+        nData += ',{\"id\":\"'+data[i].id;
+        nData +='\",\"num\":'+data[i].num+'}';
+    }
+    nData += ']';
+
+    sessionStorage.setItem('goodsData',nData);
+
 
 }
+
+/*绑定事件*/
 document.getElementById('addBtn').addEventListener('click',addGoods,false);
 document.getElementById('submit').addEventListener('click',submit,false);
